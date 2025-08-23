a3da9897c8317793e889407518492bd7eba161f2
Aniruddhakharve <120466810+Aniruddhakharve@users.noreply.github.com>
2025-08-23 16:09:59 +0000

    worked on functions in scripting

---
+++ functions&Module&Packages/01-function.py
@@ -0,0 +1,18 @@
+num1 = 25
+num2 = 34
+
+def addition():
+    add = num1 + num2
+    print (add)
+
+def substarction():
+    sub = num1 - num2
+    print (sub)
+
+def multiplication():
+    multip = num1 * num2
+    print (multip)
+
+addition()
+substarction()
+multiplication()
